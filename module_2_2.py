9990bb7b76ea22d57e90f0ab10a9e28c7aea811d
Kymblk <115482483+Kymblk@users.noreply.github.com>
2024-09-11 09:30:03 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,33 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите треть число: '))
+if first == second == third:
+    print('3')
+elif first == second:
+    print('2')
+elif first == third:
+    print('2')
+elif second == third:
+    print('2')
+else:
+    print('0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sort_s = sorted(students)
+# average = [(sum(i)/len(i)) for i in grades]
+# dict_1 = dict(zip(sort_s,average))
+# print(dict_1)
+
+
