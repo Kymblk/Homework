73a3856107404c50f5c44c090443bbb68d06912d
Kymblk <115482483+Kymblk@users.noreply.github.com>
2024-09-25 00:28:14 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,16 @@
 def get_multiplied_digits(number):
-    str_number = str(int(number))
+    if number == 0:
+        return 0
+    str_number = str(int(number)).replace('0','')
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    elif first == 0:
-        return 1
     else:
         return first
 
 print(get_multiplied_digits(40203))
+print(get_multiplied_digits('00203'))
+print(get_multiplied_digits('20300'))
+print(get_multiplied_digits(400))
+print(get_multiplied_digits(0))
 
